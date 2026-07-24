26e4e446afa2c3cde2b6647f76cc63e6fd1f9502
nino2767 <nino2767@gmail.com>
2026-07-24 21:49:16 +0900

    style: 어드민 프론트엔드(admin-view) Tailwind CSS 4 패키지 설치 및 글로벌 스타일 연결로 깨짐 현상 완전 해결

--- admin-view/.next/dev/server/chunks/ssr/_next-internal_server_app_verification-history_page_actions_053gya7.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/verification-history/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_verification-history_page_actions_053gya7.js.map
